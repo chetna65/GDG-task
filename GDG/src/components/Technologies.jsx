378348d6048d3d30004066cebb0e491746c7fd62
Chetna Deshmukh <chetnadeshmukh65@gmail.com>
2025-09-23 01:22:30 +0530

    final touches

--- GDG/src/components/Technologies.jsx
+++ GDG/src/components/Technologies.jsx
@@ -1,25 +1,33 @@
 export default function Technologies() {
-  const techs = [
-    { name: "Android", img: "android.png" },
-    { name: "Flutter", img: "flutter.png" },
-    { name: "Firebase", img: "firebase.png" },
-    { name: "Cloud", img: "cloud.png" },
-  ];
+  // const techs = [
+  //   { name: "Android", img: "android.png" },
+  //   { name: "Flutter", img: "flutter.png" },
+  //   { name: "Firebase", img: "firebase.png" },
+  //   { name: "Cloud", img: "cloud.png" },
+  // ];
 
   return (
-    <section className="py-16 text-center">
-      <h2 className="text-2xl font-semibold mb-8">Technologies</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 max-w-4xl mx-auto">
-        {techs.map((t, i) => (
-          <div
-            key={i}
-            className="p-6 bg-white dark:bg-gray-800 shadow rounded"
-          >
-            <img src={t.img} alt={t.name} className="mx-auto w-16 mb-4" />
-            <p>{t.name}</p>
-          </div>
-        ))}
-      </div>
-    </section>
+    <div>
+      <img src="/red-lines.png" className="absolute top-[4538px] left-[82px]"></img>
+      <p className="absolute top-[4485px] left-[518px] font-[Product Sans] font-bold text-[70px] leading-[150%] tracking-[-2.2%]">Technologies</p>
+      <p className="absolute top-[4590px] left-[389px] font-[Product Sans] font-normal text-[32px] leading-[150%] tracking-[-1.1%]">Domains That Excite Us to Collaborate and Teach!</p>
+
+    </div>
+    
+
+    // <section className="py-16 text-center">
+    //   <h2 className="text-2xl font-semibold mb-8">Technologies</h2>
+    //   <div className="grid grid-cols-2 md:grid-cols-4 gap-6 max-w-4xl mx-auto">
+    //     {techs.map((t, i) => (
+    //       <div
+    //         key={i}
+    //         className="p-6 bg-white dark:bg-gray-800 shadow rounded"
+    //       >
+    //         <img src={t.img} alt={t.name} className="mx-auto w-16 mb-4" />
+    //         <p>{t.name}</p>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </section>
   );
 }
