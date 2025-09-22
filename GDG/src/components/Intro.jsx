d977ff8fd1d63095487c4bcc29fa880d687bd751
Chetna Deshmukh <chetnadeshmukh65@gmail.com>
2025-09-22 21:27:02 +0530

    creating intro

--- GDG/src/components/Intro.jsx
+++ GDG/src/components/Intro.jsx
@@ -12,12 +12,12 @@ export default function Intro()
         &nbsp;
         <span>Developer Groups</span>
       </div>
-      <img src="" alt="Hero" className="mx-auto mt-8 w-80" />
       <p className="absolute top-[283px] left-[551px] font-[Product Sans] font-bold text-[50px] leading-[100%] tracking-[0%] text-[#646464]">RBU Chapter</p>
-      <p className="mt-4 max-w-xl mx-auto text-gray-600 dark:text-gray-300">
-        A community of developers to learn, share, and grow together.
+      <img src="/OBJECTS.png" alt="Hero" className="absolute top-[395px] left-[414px] h-[373px] w-[613px] " />
+      <p className="absolute top-[790px] left-[272px] text-gray-600 dark:text-gray-300 font-[Product Sans] font-normal text-[35px] leading-[100%] tracking-[3%]">
+        Google Developer Student Clubs are community groups for college<br></br> and university students interested in Google developer<br></br> technologies.
       </p>
-      
+      <button className="bg-[#4285F4] font-bold text-[30px]">JOIN US</button>
     </section>
   );
 }
